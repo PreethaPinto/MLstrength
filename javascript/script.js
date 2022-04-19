5430aa8f73ac3320c26f45edccbe91903b8f5a3f
PreethaPinto <97413508+PreethaPinto@users.noreply.github.com>
2022-04-19 12:49:26 +1000

    Added responsive hamburger menu

--- javascript/script.js
+++ javascript/script.js
@@ -1,13 +1,21 @@
-const hamburger = document.querySelector(".hamburger");
-const navMenu = document.querySelector(".nav-menu");
+$(function () {
+    $("#header").load("html/header.html");
+  });
+  
+  setTimeout(() => {
+    const hamburger = document.querySelector(".hamburger");
+    const navMenu = document.querySelector(".nav-menu");
+    
+    hamburger.addEventListener("click", () => {
+        hamburger.classList.toggle("active");
+        navMenu.classList.toggle("active");
+    })
+    
+    document.querySelectorAll(".nav-linkl").forEach(n => n.
+    addEventListener("click", () => {
+        hamburger.classList.remove("active");
+        navMenu.classList.remove("active");
+    }));
+  }, 5000);
 
-hamburger.addEventListener("click", () => {
-    hamburger.classList.toggle("active");
-    navMenu.classList.toggle("active");
-})
-
-document.querySelectorAll(".nav-linkl").forEach(n => n.
-addEventListener("click", () => {
-    hamburger.classList.remove("active");
-    navMenu.classList.remove("active");
-}))
+  
